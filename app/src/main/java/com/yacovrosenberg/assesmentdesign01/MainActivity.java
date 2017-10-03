ecb74665ff8b792c8b5e6c8065e4e68a0926e215
Yacov Rosenberg <yacov94@yahoo.com>
2017-10-03 12:59:14 -0300

    add badLAYOUT

--- app/src/main/java/com/yacovrosenberg/assesmentdesign01/MainActivity.java
+++ app/src/main/java/com/yacovrosenberg/assesmentdesign01/MainActivity.java
@@ -1,13 +1,26 @@
 package com.yacovrosenberg.assesmentdesign01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button bad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bad = (Button) findViewById(R.id.bad);
+
+        bad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intenet = new Intent(getApplicationContext(), BadActivity.class);
+                startActivity(intenet);
+            }
+        });
     }
 }
